05bcc3ecff1cc107555a9256adacea6406c5f812
unknown <olawaleprofjobs15@gmail.com>
2024-10-01 00:25:06 -0700

    modified tailwind.cofig, install packages and create variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: 'Orbitron',
+      secondary: 'Raleway',
+      tertiary: 'Montserrat',
+    },
+    container: {
+      padding:{
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#00a0a0a',
+        accent: '#BB09C3',
+      },
+      backgroundImage: {
+        'gradient-to-r': 'to right, rgba(0, 160, 160, 0.5), rgba(255, 255, 255, 0.5)',
+      }
+    },
   },
   plugins: [],
 }
